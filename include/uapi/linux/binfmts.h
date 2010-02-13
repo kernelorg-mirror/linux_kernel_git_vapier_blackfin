25f2bcfa6c8a9e7cee2b92a823495479fe0ccc92
Mike Frysinger <vapier@gentoo.org>
2010-02-13 03:09:23 -0500

    convert PAGE_SIZE usage
    The size of a page may change at runtime or based on kernel settings, so
    a static value at compile time doesn't work.  More importantly, no one
    exports PAGE_SIZE to user space anymore.
    
    URL: http://bugs.gentoo.org/301431
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- include/uapi/linux/binfmts.h
+++ include/uapi/linux/binfmts.h
@@ -2,6 +2,7 @@
 #ifndef _UAPI_LINUX_BINFMTS_H
 #define _UAPI_LINUX_BINFMTS_H
 
+#include <unistd.h>
 #include <linux/capability.h>
 
 struct pt_regs;
@@ -12,7 +13,7 @@ struct pt_regs;
  * prevent the kernel from being unduly impacted by misaddressed pointers.
  * MAX_ARG_STRINGS is chosen to fit in a signed 32-bit integer.
  */
-#define MAX_ARG_STRLEN (PAGE_SIZE * 32)
+#define MAX_ARG_STRLEN (sysconf(_SC_PAGESIZE) * 32)
 #define MAX_ARG_STRINGS 0x7FFFFFFF
 
 /* sizeof(linux_binprm->buf) */
